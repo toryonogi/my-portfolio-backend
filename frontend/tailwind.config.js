c429475b2823dfac23f21c469f359d93ce797bf5
toryonogi <newly777@gmail.com>
2026-01-31 15:06:59 +0900

    Complete: frontend構成をassetsフォルダ管理に移行しビルド設定を更新

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './index.html', // frontend直下のhtml
+    './src/**/*.{html,js}', // srcフォルダの中
+    './assets/**/*.js', // assets内のjsでクラスを使う場合
+  ],
   theme: {
     extend: {},
   },
